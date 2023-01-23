b9522ae315cd672a769a9ab51b8123f13b2e09f5
martin <martin.vrbka95@gmail.com>
2023-01-23 16:36:12 +0100

    updated script

--- main.py
+++ main.py
@@ -29,13 +29,30 @@ class DockerGUIApp(QMainWindow):
         self.connect_button.clicked.connect(self.connect_to_multidocker)
         layout.addWidget(self.connect_button)
 
-        self.list_containers_button = QPushButton("List Containers")
-        self.list_containers_button.clicked.connect(self.list_docker_containers)
-        layout.addWidget(self.list_containers_button)
+        self.container_input = QLineEdit()
+        self.container_input.setPlaceholderText("Enter container number")
+        self.container_input.setEnabled(False)  # disable input field until connection is established
+        layout.addWidget(self.container_input)
 
-        self.command_button = QPushButton("Apply Command")
-        self.command_button.clicked.connect(self.apply_docker_command)
-        layout.addWidget(self.command_button)
+        self.start_button = QPushButton("Start")
+        self.start_button.clicked.connect(self.start_container)
+        self.start_button.setEnabled(False)  # disable button until container number is entered
+        layout.addWidget(self.start_button)
+
+        self.stop_button = QPushButton("Stop")
+        self.stop_button.clicked.connect(self.stop_container)
+        self.stop_button.setEnabled(False)  # disable button until container number is entered
+        layout.addWidget(self.stop_button)
+
+        self.restart_button = QPushButton("Restart")
+        self.restart_button.clicked.connect(self.restart_container)
+        self.restart_button.setEnabled(False)  # disable button until container number is entered
+        layout.addWidget(self.restart_button)
+
+        self.logs_button = QPushButton("Logs")
+        self.logs_button.clicked.connect(self.logs_container)
+        self.logs_button.setEnabled(False)  # disable button until container number is entered
+        layout.addWidget(self.logs_button)
 
         self.container_list = QListWidget()
         layout.addWidget(self.container_list)
@@ -43,7 +60,6 @@ class DockerGUIApp(QMainWindow):
         # Add the layout to the central widget
         central_widget.setLayout(layout)
 
-    # rest of the code...
     def connect_to_multidocker(self):
         address = self.address_input.text()
         username = self.username_input.text()
@@ -51,10 +67,9 @@ class DockerGUIApp(QMainWindow):
 
         # Connect to the server using Fabric
         self.conn = Connection(host=address, user=username, connect_kwargs={"password": password})
-        self.conn.run()
+        self.conn.run("")
 
-    def list_docker_containers(self):
-        # Run the "docker ps" command
+# Run the "docker ps" command to list available containers
         result = self.conn.run("docker ps")
         # Split the output by newline and remove the first and last element
         container_list = result.stdout.split("\n")[1:-1]
@@ -65,30 +80,37 @@ class DockerGUIApp(QMainWindow):
         for container in container_list:
             self.container_list.addItem(container)
 
-    def apply_docker_command(self):
-        command = self.select_docker_command()
-        selected_container = self.container_list.currentItem().text().split()[0]
-        # Run the command on the selected container
-        self.conn.run(f"docker {command} {selected_container}")
+        self.container_input.setEnabled(True)  # enable container input field
+        self.container_input.textChanged.connect(self.enable_container_buttons)
 
-    def select_docker_command(self):
-        print("Select a Docker command:")
-        print("1. start")
-        print("2. stop")
-        print("3. restart")
-        print("4. logs")
-        command = input()
-        if command == "1":
-            return "start"
-        elif command == "2":
-            return "stop"
-        elif command == "3":
-            return "restart"
-        elif command == "4":
-            return "logs"
+    def enable_container_buttons(self):
+        # check if container number is entered in the input field
+        if self.container_input.text().strip() != "":
+            self.start_button.setEnabled(True)
+            self.stop_button.setEnabled(True)
+            self.restart_button.setEnabled(True)
+            self.logs_button.setEnabled(True)
         else:
-            print("Invalid command. Try again.")
-            self.select_docker_command()
+            self.start_button.setEnabled(False)
+            self.stop_button.setEnabled(False)
+            self.restart_button.setEnabled(False)
+            self.logs_button.setEnabled(False)
+
+    def start_container(self):
+        container = self.container_input.text()
+        self.conn.run(f"docker start {container}")
+
+    def stop_container(self):
+        container = self.container_input.text()
+        self.conn.run(f"docker stop {container}")
+
+    def restart_container(self):
+        container = self.container_input.text()
+        self.conn.run(f"docker restart {container}")
+
+    def logs_container(self):
+        container = self.container_input.text()
+        self.conn.run(f"docker logs {container}")
 
     def closeEvent(self, event):
         self.conn.close()
